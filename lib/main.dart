b0d42919c137a15a2a1403441a02d5d568f4a5fc
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-09-29 14:32:49 -0400

    create Header

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shopping App',
       theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromRGBO(254, 206, 1, 1),
-          )),
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1)),
+      ),
       home: const HomePage(),
     );
   }
