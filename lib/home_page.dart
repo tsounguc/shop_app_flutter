4a131aa130a186403bd97c594a75161cd8496a89
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-01 19:58:50 -0400

    Add filter bar, images and product dummy data

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<String> filters = const ['All', 'Adidas', 'Nike', 'Bata'];
+  late String selectedFilter;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +55,35 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: filters.length,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: GestureDetector(
+                      onTap: (){setState((){selectedFilter = filter;});},
+                      child: Chip(
+                        backgroundColor: selectedFilter == filter ? Theme.of(context).colorScheme.primary: const Color.fromRGBO(245, 247, 249, 1),
+                        side: const BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                        label: Text(filter),
+                        labelStyle: const TextStyle(fontSize: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
