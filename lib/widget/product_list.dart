965d1830406d2521675ccff8ed4836ca41320ecd
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-08 06:19:58 -0500

    Use LayoutBuilder builder because its constrainsts are based on the parent widget sizing

--- lib/widget/product_list.dart
+++ lib/widget/product_list.dart
@@ -93,58 +93,62 @@ class _ProductListState extends State<ProductList> {
             ),
           ),
           Expanded(
-              child: size.width > 650
-                  ? GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2, childAspectRatio: 2.0),
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        final product = products[index];
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return ProductDetailsPage(product: product);
-                                },
-                              ),
-                            );
+            child: LayoutBuilder(builder: (context, constraints) {
+              if (constraints.maxWidth > 650) {
+                return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 2.0),
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      final product = products[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return ProductDetailsPage(product: product);
+                              },
+                            ),
+                          );
+                        },
+                        child: ProductCard(
+                          title: product['title'] as String,
+                          price: product['price'] as double,
+                          image: product['imageUrl'] as String,
+                          backgroundColor: index.isEven
+                              ? const Color.fromRGBO(216, 240, 253, 1)
+                              : const Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                      );
+                    });
+              }
+              return ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return ProductDetailsPage(product: product);
                           },
-                          child: ProductCard(
-                            title: product['title'] as String,
-                            price: product['price'] as double,
-                            image: product['imageUrl'] as String,
-                            backgroundColor: index.isEven
-                                ? const Color.fromRGBO(216, 240, 253, 1)
-                                : const Color.fromRGBO(245, 247, 249, 1),
-                          ),
-                        );
-                      })
-                  : ListView.builder(
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        final product = products[index];
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return ProductDetailsPage(product: product);
-                                },
-                              ),
-                            );
-                          },
-                          child: ProductCard(
-                            title: product['title'] as String,
-                            price: product['price'] as double,
-                            image: product['imageUrl'] as String,
-                            backgroundColor: index.isEven
-                                ? const Color.fromRGBO(216, 240, 253, 1)
-                                : const Color.fromRGBO(245, 247, 249, 1),
-                          ),
-                        );
-                      },
-                    ))
+                        ),
+                      );
+                    },
+                    child: ProductCard(
+                      title: product['title'] as String,
+                      price: product['price'] as double,
+                      image: product['imageUrl'] as String,
+                      backgroundColor: index.isEven
+                          ? const Color.fromRGBO(216, 240, 253, 1)
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                    ),
+                  );
+                },
+              );
+            }),
+          ),
         ],
       ),
     );
